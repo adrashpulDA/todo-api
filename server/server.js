1564e361420f1f16d8cf44f7ff3d8c1d676edf09
Artem Drashpul <adrashpul@dataart.com>
2017-12-07 12:26:03 +0100

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 // c:\Program Files\MongoDB\Server\3.4\bin>mongod.exe --dbpath D:\Projects\Node\mongo-db-data
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
-
-var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+const _ = require('lodash');
 
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -61,9 +61,41 @@ app.delete('/todos/:id', (req, res) => {
         if (!todo) {
             res.status(404).send();
         }
-        res.status(200).send({todo});
+        res.status(200).send({ todo });
     }).catch(err => {
         res.status(404).send();
+    });
+});
+
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params['id'];
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send('Id is not valid');
+    }
+
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(
+        id,
+        { $set: body },
+        { new: true }
+    ).then(todo => {
+        if (!todo) {
+            res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch(err => {
+        res.status(400).send(err);
     });
 });
 
