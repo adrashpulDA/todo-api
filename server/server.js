c726af513f9ac9aa4d71f80c21959f86167789f3
Artem Drashpul <adrashpul@dataart.com>
2017-12-18 22:47:51 +0100

    added login message

--- server/server.js
+++ server/server.js
@@ -121,11 +121,26 @@ app.post('/users', (req, res) => {
         .catch(e => res.status(400).send(e));
 });
 
-
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
-
 });
+
+//POST /user/login (eamil, password)
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = User.findByCredentials(body.email, body.password)
+        .then(user => {
+            return user.generateAuthToken()
+                .then(token => {
+                    res
+                        .header('x-auth', token)
+                        .send(user);
+                })
+        })
+        .catch(e => res.status(400).send(e));
+});
+
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
